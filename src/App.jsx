6ab49a490ba65947e154f07a08ed9a328b07c7ef
Akshay Pratap Singh <mrprabalpratap23@gmail.com>
2026-04-08 14:42:21 +0530

    Added new components and fixed CSS layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,52 @@
+// import { Routes, Route } from "react-router-dom";
+
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Strengths from "./components/Strengths";
+// import Products from "./components/Products";
+// import ProductDetails from "./components/ProductDetails";
+// import Certifications from "./components/Certifications";
+// import Coverage from "./components/Coverage";
+// import Footer from "./components/Footer";
+// import WhatsappFloat from "./components/WhatsappFloat";
+// import MapFloat from "./components/mapFloat";
+// import Motive from "./components/Motive";
+
+
+
+// function HomePage() {
+//   return (
+//     <>
+//       <Hero />
+//       <About />
+//       <Motive />
+//       <Strengths />
+//       <Products />
+//       <Certifications />
+//       <Coverage />
+//       <Footer />
+//       <WhatsappFloat />
+//       <MapFloat /> 
+//     </>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/product/:name" element={<ProductDetails />} />
+//       </Routes>
+
+//     </>
+//   );
+// }
+
+// export default App;
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
@@ -12,6 +61,7 @@ import Footer from "./components/Footer";
 import WhatsappFloat from "./components/WhatsappFloat";
 import MapFloat from "./components/mapFloat";
 import Motive from "./components/Motive";
+
 function HomePage() {
   return (
     <>
@@ -31,7 +81,9 @@ function HomePage() {
 
 function App() {
   return (
-    <>
+    /* 👇 The Bulletproof Wrapper 👇 */
+    <div style={{ overflowX: "hidden", width: "100%", maxWidth: "100vw", position: "relative" }}>
+      
       <Navbar />
 
       <Routes>
@@ -39,7 +91,7 @@ function App() {
         <Route path="/product/:name" element={<ProductDetails />} />
       </Routes>
 
-    </>
+    </div>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,18 +46,7 @@ export default function Footer() {
         </div>
 
 
-        <div className="footer-col">
-
-          <h4>Quick Links</h4>
-
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Products</li>
-            <li>Contact Us</li>
-          </ul>
-
-        </div>
+       
 
 
         <div className="footer-col">
